2fa362101dd5930ac58d6aa933a79cf5b815fd93
Chyld Medford <chyld.medford@gmail.com>
2014-12-09 10:30:24 -0600

    initial

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict';
+
+  angular.module('starter.controllers', [])
+
+  .controller('DashCtrl', function($scope){
+  })
+
+  .controller('FriendsCtrl', function($scope, Friends){
+    $scope.friends = Friends.all();
+  })
+
+  .controller('FriendDetailCtrl', function($scope, $stateParams, Friends){
+    $scope.friend = Friends.get($stateParams.friendId);
+  })
+
+  .controller('AccountCtrl', function($scope){
+  });
+
+})();
---
+++ www/js/services.js
@@ -0,0 +1,22 @@
+(function(){
+  'use strict';
+
+  angular.module('starter.services', [])
+  .factory('Friends', function(){
+    var friends = [
+      {id: 0, name: 'Scruff McGruff'},
+      {id: 1, name: 'G.I. Joe'},
+      {id: 2, name: 'Miss Frizzle'},
+      {id: 3, name: 'Ash Ketchum'}
+    ];
+    return {
+      all: function(){
+        return friends;
+      },
+      get: function(friendId){
+        return friends[friendId];
+      }
+    };
+  });
+
+})();
