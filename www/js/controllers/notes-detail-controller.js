258849d170364ccbd016336385343be0ab4f3434
Chyld Medford <chyld.medford@gmail.com>
2014-12-10 11:58:35 -0600

    save photo wip

--- www/js/controllers/notes-detail-controller.js
+++ www/js/controllers/notes-detail-controller.js
@@ -8,6 +8,33 @@
       $scope.moment = moment;
       $scope.note = response.data;
     });
+
+    function success(b64){
+      console.log(b64);
+    }
+
+    function error(msg){
+      console.log(msg);
+    }
+
+    $scope.snap = function(){
+      var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.DATA_URL
+      };
+
+      navigator.camera.getPicture(success, error, options);
+    };
+
+    $scope.choose = function(){
+      var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+      };
+
+      navigator.camera.getPicture(success, error, options);
+    };
   });
 
 })();
